07802e2cf41fb3bd99646918b8311c4bdfe5fac4
배성윤 <129399109+Bae309@users.noreply.github.com>
2023-09-21 00:19:15 +0900

    Add files via upload

---
+++ 1강/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello,\tWorld!");
+        Console.Write("Hello\nWorld!");
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine(DateTime.Now.Hour < 3 || 8 < DateTime.Now.Hour);
+        Console.WriteLine(3 < DateTime.Now.Hour && DateTime.Now.Hour < 8);
+    }
+}
