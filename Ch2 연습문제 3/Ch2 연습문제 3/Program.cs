1137bf955afe6a63cccfe5f7fa7e96fd3e13ef21
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:14:00 +0900

    Add files via upload

---
+++ Ch2 연습문제 3/Ch2 연습문제 3/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("원의 반지름을 입력하시오 : ");
+        string a = Console.ReadLine();
+        double len = double.Parse(a);
+
+        double pi = 3.14;
+
+        double round = 2 * pi * len;
+        double area = pi * len * len;
+
+        Console.Write("둘레 = ");
+        Console.WriteLine(round.ToString("0.000"));
+
+        Console.Write("넓이 = ");
+        Console.WriteLine(area.ToString("0.000"));
+
+
+    }
+}
