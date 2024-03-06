e384917540a0e075f19adca47f6455ab192b14c4
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:44:17 +0900

    Add files via upload

---
+++ p087/p087/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string message = "안녕하세요";
+
+        Console.WriteLine(message + "!");
+        Console.WriteLine(message[0]);
+        Console.WriteLine(message[1]);
+        Console.WriteLine(message[2]);
+    }
+}
---
+++ p091/p091/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int output = 0;
+        output += 52;
+        output += 273;
+        output += 103;
+
+        Console.WriteLine(output);
+
+        Console.WriteLine();
+
+        int output2 = 0;
+        output2 = output2 + 52;
+        output2 = output2 + 273;
+        output2 = output2 + 103;
+
+        Console.WriteLine(output2);
+
+    }
+}
