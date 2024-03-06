d14e4d0ca59fe8b5a112a6db82a41eddfabe0c3c
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:29:47 +0900

    Add files via upload

---
+++ Ch4 연습문제/ch4 연습문제/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i = 0;
+        while (true)
+        {
+            Console.Write("나이 입력(종료 : exit) : ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 9)
+            {
+                Console.WriteLine("입장 불가!");
+                Console.WriteLine("-------------------------------------------");
+            }
+
+            else
+            {
+                Console.Write("키 입력 : ");
+                int height = int.Parse(Console.ReadLine());
+                if (height < 130)
+                {
+                    Console.WriteLine("저속 롤러코스터 입장 가능");
+                    Console.WriteLine("----------------------------------------")
+                }
+
+                else
+                {
+                    Console.WriteLine("고속 롤러코스터 입장 가능");
+                    Console.WriteLine("----------------------------------------")
+                }
+
+            }
+
+        }
+    }
+}
