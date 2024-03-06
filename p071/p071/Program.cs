dd814064f18b6cadd535cf60491883ac680fbe37
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:37:25 +0900

    Add files via upload

---
+++ p071/p071/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("가나다" + "라마" + "바사아" + "자차카타" + "파하");
+
+        Console.WriteLine("------------------------------------------------------");
+
+        Console.WriteLine("안녕하세요"[0]);
+        Console.WriteLine("안녕하세요"[1]);
+        Console.WriteLine("안녕하세요"[3]);
+    }
+}
---
+++ p073/p073/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine('가' + '힣');
+
+        Console.WriteLine("-------------------------------------");
+
+        Console.WriteLine(true);
+        Console.WriteLine(false);
+    }
+}
