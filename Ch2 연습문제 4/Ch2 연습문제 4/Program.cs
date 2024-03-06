be1e92ec2afc0620414e9be7b42b8e1d30265743
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:17:48 +0900

    Add files via upload

---
+++ Ch2 연습문제 4/Ch2 연습문제 4/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("문자 입력 : ");
+        int a1 = Console.Read();
+        char b1 = Convert.ToChar(a1);
+
+        char b2 = Convert.ToChar(a1 + 5);
+
+        Console.WriteLine("1. 연산 수행 전 ");
+        Console.WriteLine("- 아스키코드 값 : " + a1);
+        Console.WriteLine("- 변환된 문자 출력 : " + b1);
+
+        Console.WriteLine("1. 연산 수행 전 ");
+        Console.WriteLine("- 아스키코드 값 : " + (a1 + 5));
+        Console.WriteLine("- 변환된 문자 출력 : " + b2);
+
+    }
+}
