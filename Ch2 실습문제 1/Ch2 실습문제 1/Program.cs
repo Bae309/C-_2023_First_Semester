4658c22ed9cf6d11bbb0def041cf61f5f8db2804
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:10:50 +0900

    Add files via upload

---
+++ Ch2 실습문제 1/Ch2 실습문제 1/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("변환할 정수 하나를 입력하시오 : ");
+        string a = Console.ReadLine();
+        double num = double.Parse(a);
+
+        Console.WriteLine(num + "cm = " + (num * 2.54));
+    }
+}
