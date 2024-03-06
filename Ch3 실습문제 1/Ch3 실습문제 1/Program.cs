e268bfc4ad893139cf76e6f324184349ad310fe0
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:19:08 +0900

    Add files via upload

---
+++ Ch3 실습문제 1/Ch3 실습문제 1/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("첫번째 숫자 입력 : ");
+        string a = Console.ReadLine();
+        Console.Write("두번째 숫자 입력 : ");
+        string b = Console.ReadLine();
+
+        int num1 = int.Parse(a);
+        int num2 = int.Parse(b);
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("두 수 중에 큰 수는 " + num1 + "이고, 작은 수는 " + num2 + "이다");
+        }
+
+        if (num2 > num1)
+        {
+            Console.WriteLine("두 수 중에 큰 수는 " + num2 + "이고, 작은 수는 " + num1 + "이다");
+        }
+
+    }
+}
---
+++ Ch3 실습문제 2/Ch3 실습문제 2/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("숫자 입력 : ");
+        string a = Console.ReadLine();
+        int num = int.Parse(a);
+
+        if (num < 0)
+        {
+            Console.WriteLine("입력 값 " + num + "는(은) 음수이다.");
+        }
+
+        if (num == 0)
+        {
+            Console.WriteLine("입력 값 " + num + "은 Zero이다.");
+        }
+
+        if (num > 0)
+        {
+            Console.WriteLine("입력 값 " + num + "는(은) 양수이다.");
+        }
+    }
+}
