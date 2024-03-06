01da03201efa10163237a9bf7105b4f45332f02c
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:51:24 +0900

    Add files via upload

---
+++ p116/p116/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(bool.Parse("True"));
+        Console.WriteLine(bool.Parse("true"));
+        Console.WriteLine(bool.Parse("False"));
+        Console.WriteLine(bool.Parse("false"));
+    }
+}
---
+++ p129/p129/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (DateTime.Now.Hour < 12)
+        {
+            Console.WriteLine("오전입니다.");
+        }
+
+        if (12 <= DateTime.Now.Hour)
+        {
+            Console.WriteLine("오후입니다.");
+        }
+    }
+}
