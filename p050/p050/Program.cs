fa77ff2f692eb4e8f8177688f55f8816f7030ae0
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:31:29 +0900

    Add files via upload

---
+++ p050/p050/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Write"); Console.Write("Write");
+        Console.Write("Write");
+        Console.Write("Write");
+        Console.Write("Write");
+
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.WriteLine("WriteLine");
+        Console.WriteLine("WriteLine");
+        Console.WriteLine("WriteLine");
+        Console.WriteLine("WriteLine");
+        Console.WriteLine("WriteLine");
+    }
+}
---
+++ p051/p051/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Write");
+        Console.WriteLine("WriteLine");
+
+        Console.WriteLine("WriteLine");
+        Console.WriteLine("WriteLine");
+
+        Console.Write("Write");
+        Console.WriteLine("Write");
+    }
+}
