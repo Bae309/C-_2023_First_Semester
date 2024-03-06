12b959414b6ad764d995b7232e5ca61f330d03e5
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:13:19 +0900

    Add files via upload

---
+++ Ch2 실습문제 2/ch2 실습문제 2/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("1. 첫번째 정수 입력 : ");
+        string a = Console.ReadLine();
+
+        Console.Write("2. 두번쨰 정수 입력 : ");
+        string b = Console.ReadLine();
+
+        double num1 = double.Parse(a);
+        double num2 = double.Parse(b);
+        double result = num1 / num2;
+
+        Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+        Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+        Console.WriteLine(num1 + " * " + num2 + " = " + (num1 * num2));
+        Console.Write(num1 + " / " + num2 + " = ");
+        Console.WriteLine(result.ToString("0.0"));
+
+
+    }
+}
