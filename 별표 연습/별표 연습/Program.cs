05e6be8692b179878c9aad14ef46815de68bc236
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-03-06 21:53:41 +0900

    Add files via upload

---
+++ 별표 연습/별표 연습/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("피라미드 높이를 입력하시오 : ");
+        int height = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i < height; i++)
+        {
+            for (int j = 1; j <= height - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 1; j <= (2 * i) - 1; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
